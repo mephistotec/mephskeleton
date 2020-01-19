e110d8dd2af14e64e57b8ef9162602a4241ecedb
Daniel Luque <daniel.luque@mango.com>
2020-01-19 15:05:34 +0100

    Modifs portatil

--- sample/mephmicro/build_pipeline/02_docker_build_images.sh
+++ sample/mephmicro/build_pipeline/02_docker_build_images.sh
@@ -3,25 +3,23 @@
 
 basePath=$(pwd)
 
-function build_image_for_java
-{
-    echo "building image for $1"
-    pushd ../$1
 
-    ARTIFACT=$(mvn help:evaluate -Dexpression=project.build.finalName | grep -e '^[^\[]')
+function splitJarIntoFrameworkAndCode
+{
+    ARTIFACT=$1
+    WRKDIR=$2
+    ORIGIN=$3
     FRAMEWORK_JAR_NAME=$ARTIFACT"_framework"
     ARTIFACT_JAR=$ARTIFACT"_code"
-    popd
 
-    BASEDOCKERDIR=./docker_images_definitions/java
-
-    rm $BASEDOCKERDIR/tmp_for_jars/*.jar
-    mkdir -p $BASEDOCKERDIR/tmp_for_jars/decomp
-    rm -R $BASEDOCKERDIR/tmp_for_jars/decomp/*
+    echo "Building image, decompressing $ARTIFACT.jar with $WRKDIR and $ORIGIN"
+    mkdir -p $WRKDIR/decomp
+    rm -R $WRKDIR/decomp/*
     
     echo "Building image, looking for ../$1/target/$ARTIFACT.jar"
-    cp ../$1/target/$ARTIFACT.jar $BASEDOCKERDIR/tmp_for_jars/decomp
+    cp $ORIGIN/$ARTIFACT.jar $WRKDIR/decomp
     echo "Building image, decompressing $ARTIFACT.jar"
+
     pushd $BASEDOCKERDIR/tmp_for_jars/decomp
       unzip ./$ARTIFACT.jar;
       rm  ./$ARTIFACT.jar;
@@ -30,19 +28,61 @@ function build_image_for_java
       rm ../framework_content.txt
       find . -type f | grep -v lib | grep -v "org/springframework/boot/loader" > ../micro_content.txt;find . -type f | grep lib | grep mephmicro >> ../micro_content.txt
       find . -type f | grep "org/springframework/boot/loader" > ../framework_content.txt; find . -type f | grep lib | grep -v mephmicro >> ../framework_content.txt
-
-      #echo "-----------------------------------"
-      #cat ../micro_content.txt  | sort | uniq -c
-      #echo "-----------------------------------"
-      #cat ../framework_content.txt  | sort | uniq -c
-
       echo "Building image, zipping service code into $(pwd)/../$ARTIFACT_JAR.jar"
-      zip ../$ARTIFACT_JAR.jar -n .jar -@ < ../micro_content.txt
+      zip ../$ARTIFACT_JAR.jar -m -n .jar -@ < ../micro_content.txt
       #cat ../micro_content.txt | while read fichero; do echo $fichero;zip -m ../$ARTIFACT_JAR.jar $fichero;done;
       echo "Building image, zipping service code into $(pwd)/$FRAMEWORK_JAR_NAME.jar"
-      zip ../$FRAMEWORK_JAR_NAME.jar -n .jar -@ < ../framework_content.txt
+      zip ../$FRAMEWORK_JAR_NAME.jar -m -n .jar -@ < ../framework_content.txt
       #cat ../framework_content.txt | while read fichero; do echo $fichero; zip -m ../$FRAMEWORK_JAR_NAME.jar $fichero;done;
     popd
+    rm -R $WRKDIR/decomp/*
+}
+
+function joinJars 
+{
+    SERVICE_JAR=$1
+    FRAMEWORK_JAR=$2
+    RESULT_JAR=$3
+    WRKDIR=$4
+
+    echo "Building fina imaje: joining jars $SERVICE_JAR $FRAMEWORK_JAR into $RESULT_JAR in $WRKDIR"
+    mkdir -p $WRKDIR/decomp
+    rm -R $WRKDIR/decomp/*
+    cp $1 $WRKDIR/decomp
+    cp $2 $WRKDIR/decomp
+  
+    pushd $WRKDIR/decomp
+      echo "------- Files to process ------"
+      ls
+      echo "------- decompressiong ------"
+      ls | while read fichero; do unzip $fichero; done;
+      unzip *.jar
+      rm *.jar
+      zip  $RESULT_JAR -m -n .jar -r *
+    popd
+}
+
+function build_image_for_java
+{
+    echo "building image for $1"
+    pushd ../$1
+    ARTIFACT=$(mvn help:evaluate -Dexpression=project.build.finalName | grep -e '^[^\[]')
+    popd
+
+    BASEDOCKERDIR=./docker_images_definitions/java
+
+    rm $BASEDOCKERDIR/tmp_for_jars/*.jar
+    mkdir -p $BASEDOCKERDIR/tmp_for_jars/decomp
+    rm -R $BASEDOCKERDIR/tmp_for_jars/decomp/*
+    
+    splitJarIntoFrameworkAndCode $ARTIFACT $BASEDOCKERDIR/tmp_for_jars ../$1/target
+
+
+    FRAMEWORK_JAR_NAME=$ARTIFACT"_framework"
+    ARTIFACT_JAR=$ARTIFACT"_code"
+
+    joinJars $BASEDOCKERDIR/tmp_for_jars/$FRAMEWORK_JAR_NAME".jar" $BASEDOCKERDIR/tmp_for_jars/$ARTIFACT_JAR".jar" $ARTIFACT"_join".jar $BASEDOCKERDIR/tmp_for_jars
+
     # rm -R $BASEDOCKERDIR/tmp_for_jars/decomp/*
 
     # rc=$?
